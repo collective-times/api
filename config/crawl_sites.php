0bd14ef540b89f063a82e379a031a221695aa411
Makoto Chiba <hypermkt@gmail.com>
2018-07-09 01:21:17 +0900

    はてなブックマーク テクノロジーの新着エントリーのRSSを追加

--- config/crawl_sites.php
+++ config/crawl_sites.php
@@ -7,4 +7,10 @@ return [
         'enabled' => true, // true: クロール対象とする false: クロール対象外とする
         'class' => '\App\ContentsParser\RSS2',
     ],
+    [
+        'crawl_url' => 'http://b.hatena.ne.jp/entrylist/it.rss',
+        'source_url' => 'http://b.hatena.ne.jp/entrylist/it',
+        'enabled' => true, // true: クロール対象とする false: クロール対象外とする
+        'class' => '\App\ContentsParser\RSS2',
+    ],
 ];
