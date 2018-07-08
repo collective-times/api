780eec2c738429b17445505a6fba2d6d4f59799b
Makoto Chiba <hypermkt@gmail.com>
2018-07-08 12:09:15 +0900

    articlesを全件返す状態でテストケースまで実装

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\DataAccess\Eloquent\Article;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        return response()->json(['articles' => [
+        // TODO: ページネーション
+        $articles = Article::all();
 
-        ]]);
+        return response()->json(['articles' => $articles->map(function($article) {
+            return [
+                'title' => $article->title,
+                'description' => $article->description,
+                'date' => $article->publish_date,
+                'articleUrl' => $article->article_url,
+                'sourceUrl' => $article->source_url,
+                'imageUrl' => $article->image_url,
+                'faviconUrl' => $article->faviron_url,
+            ];
+        })]);
     }
 }
--- tests/Feature/Api/V1/ArticleTest.php
+++ tests/Feature/Api/V1/ArticleTest.php
@@ -2,15 +2,31 @@
 
 namespace Tests\Feature\Api\V1;
 
+use App\DataAccess\Eloquent\Article;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ArticleTest extends TestCase
 {
+    use DatabaseTransactions;
+
     public function testIndex()
     {
-        $response = $this->getJson('/v1/articles');
+        factory(Article::class, 3)->create();
 
+        $response = $this->getJson('/v1/articles');
         $response->assertStatus(200);
+        $response->assertJsonCount(3, 'articles');
+        $response->assertJsonStructure(['articles' => [
+            [
+                'title',
+                'description',
+                'date',
+                'articleUrl',
+                'sourceUrl',
+                'imageUrl',
+                'faviconUrl',
+            ]
+        ]]);
     }
 }
