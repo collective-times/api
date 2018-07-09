5a16f86d0a544e20d6458e58718177a3b6dc4fea
Makoto Chiba <hypermkt@gmail.com>
2018-07-10 01:40:55 +0900

    User-Agentが無いとリクエストできないサーバー用にリクエストモードを付けた

--- config/crawl_sites.php
+++ config/crawl_sites.php
@@ -5,12 +5,14 @@ return [
         'crawl_url' => 'http://b.hatena.ne.jp/hotentry/it.rss',
         'source_url' => 'http://b.hatena.ne.jp/hotentry/it',
         'enabled' => true, // true: クロール対象とする false: クロール対象外とする
+        'mode' => 'guzzle',
         'class' => '\App\ContentsParser\RSS2',
     ],
     [
         'crawl_url' => 'http://b.hatena.ne.jp/entrylist/it.rss',
         'source_url' => 'http://b.hatena.ne.jp/entrylist/it',
         'enabled' => true, // true: クロール対象とする false: クロール対象外とする
+        'mode' => 'guzzle',
         'class' => '\App\ContentsParser\RSS2',
     ],
 ];
