fa98d1b674ff2833f3b560f67ef5ddcc4da53542
Makoto Chiba <hypermkt@gmail.com>
2018-07-12 00:29:59 +0900

    Awsブログを追加

--- config/crawl_sites.php
+++ config/crawl_sites.php
@@ -13,4 +13,10 @@ return [
         'enabled' => true, // true: クロール対象とする false: クロール対象外とする
         'class' => '\App\ContentsParser\Entity\RSS2',
     ],
+    [
+        'crawl_url' => 'https://aws.amazon.com/jp/blogs/news/feed/',
+        'source_url' => 'https://aws.amazon.com/jp/blogs/news/',
+        'enabled' => true, // true: クロール対象とする false: クロール対象外とする
+        'class' => '\App\ContentsParser\Entity\Aws',
+    ],
 ];
