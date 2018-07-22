43900c4df6cf0b8de3880172e2c4f43d010884bb
Makoto Chiba <hypermkt@gmail.com>
2018-07-22 21:43:29 +0900

    コメント調整

--- config/crawl_sites.php
+++ config/crawl_sites.php
@@ -1,22 +1,28 @@
 <?php
 
+/**
+ * crawl_url string クロールするURL
+ * source_url string ソース元URL
+ * enabled boolean クロール対象フラグ
+ * class string 処理クラス
+ */
 return [
     [
         'crawl_url' => 'http://b.hatena.ne.jp/hotentry/it.rss',
         'source_url' => 'http://b.hatena.ne.jp/hotentry/it',
-        'enabled' => true, // true: クロール対象とする false: クロール対象外とする
+        'enabled' => true,
         'class' => '\App\ContentsParser\Entity\RSS2',
     ],
     [
         'crawl_url' => 'http://b.hatena.ne.jp/entrylist/it.rss',
         'source_url' => 'http://b.hatena.ne.jp/entrylist/it',
-        'enabled' => true, // true: クロール対象とする false: クロール対象外とする
+        'enabled' => true,
         'class' => '\App\ContentsParser\Entity\RSS2',
     ],
     [
         'crawl_url' => 'https://aws.amazon.com/jp/blogs/news/feed/',
         'source_url' => 'https://aws.amazon.com/jp/blogs/news/',
-        'enabled' => true, // true: クロール対象とする false: クロール対象外とする
+        'enabled' => true,
         'class' => '\App\ContentsParser\Entity\Aws',
     ],
 ];
