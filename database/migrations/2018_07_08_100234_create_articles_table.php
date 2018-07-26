d8ee1afed76caa4289080de3bccb8f01ad84bf13
Makoto Chiba <hypermkt@gmail.com>
2018-07-26 21:19:02 +0900

    articlesテーブルとsitesテーブルのリレーションを設定する

--- database/migrations/2018_07_08_100234_create_articles_table.php
+++ database/migrations/2018_07_08_100234_create_articles_table.php
@@ -15,6 +15,8 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('site_id')->unsigned()->nullable(); // MEMO: sitesテーブルへの移行期間中のみnullableにする。開発が終わったらnullable()削除する。
+            $table->foreign('site_id')->references('id')->on('sites');
             $table->dateTime('publish_date')->nullable();
             $table->string('title');
             $table->text('description')->nullable();
