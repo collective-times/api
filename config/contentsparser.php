94a16793392c7356bf230314f1cfdd7eceb948e3
Makoto Chiba <hypermkt@gmail.com>
2018-08-17 22:11:19 +0900

    名前空間の指定が間違っていた

--- config/contentsparser.php
+++ config/contentsparser.php
@@ -2,5 +2,5 @@
 
 return [
     ['name' => 'rss', 'class' => '\App\ContentsParser\Entity\RSS2'],
-    ['name' => 'hatena', 'class' => '\App\ContentsParser\Entity\RSS2'],
+    ['name' => 'hatena', 'class' => '\App\ContentsParser\Entity\Hatena'],
 ];
