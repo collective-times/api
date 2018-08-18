e927912456e636b3cf3defc9230befdcd2c496ba
Makoto Chiba <hypermkt@gmail.com>
2018-08-18 18:08:24 +0900

    registration idを登録するエンドポイントを追加

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::prefix('v1')->namespace('Api\V1')->group(function() {
     Route::get('articles', 'ArticleController@index');
     Route::get('contents-parsers', 'ContentsParserController@index');
+    Route::post('android/devices', 'Android\DeviceController@store');
 
     Route::middleware('auth:api')->group(function () {
         Route::resource('sites', 'SiteController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
