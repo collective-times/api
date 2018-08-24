0549d63e2c5eef8c6d5bdd3abc219d4dffd96bbe
Makoto Chiba <hypermkt@paperboy.co.jp>
2018-08-24 20:34:33 +0900

    記事情報を10分間キャッシュ化する

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\DataAccess\Eloquent\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // MEMO: pageクエリパラメーターは、Laravelが自動判別する
-        // refs: https://readouble.com/laravel/5.6/ja/pagination.html
-        $articles = Article::orderBy('publish_date', 'desc')->paginate(10);
+        $page = $request->page ?? 1;
+        $key = 'articles_page_' . $page;
+
+        if (Cache::has($key)) {
+            $articles = Cache::get($key);
+        } else {
+            // MEMO: pageクエリパラメーターは、Laravelが自動判別する
+            // refs: https://readouble.com/laravel/5.6/ja/pagination.html
+            $articles = Article::orderBy('publish_date', 'desc')->paginate(10);
+
+            Cache::put($key, $articles, now()->addMinutes(10));
+        }
 
         return response()->json(['articles' => $articles->map(function($article) {
             return [
