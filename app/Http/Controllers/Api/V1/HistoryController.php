2dff0bc044b3ba5468b3c1bbe94e757994f9f12a
Makoto Chiba <hypermkt@gmail.com>
2018-09-07 01:24:17 +0900

    JWTのペイロードにuser_idがある場合はそれを登録する

--- app/Http/Controllers/Api/V1/HistoryController.php
+++ app/Http/Controllers/Api/V1/HistoryController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Repositories\ArticleUserRepository;
+use Firebase\JWT\JWT;
 
 class HistoryController extends Controller
 {
@@ -48,13 +49,16 @@ class HistoryController extends Controller
      */
     public function store(Request $request)
     {
-         if (Auth::check()) {
-            Auth::user()->articles()->attach($request->article_id);
-        } else {
-             ArticleUser::create([
-                 'article_id' => $request->article_id
-             ]);
+        $authorization = $request->header('Authorization');
+        if ($authorization) {
+            $jwt = substr($authorization, 7);
+            $payload = JWT::decode($jwt, config('passport.public_keyt'), ['RS256']);
         }
+
+        ArticleUser::create([
+            'user_id' => $payload->sub ?? null,
+            'article_id' => $request->article_id
+        ]);
 
         return response()->json([
             'article_id' => $request->article_id,
