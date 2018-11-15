8cb10e1bffeb5bf769d9f1d043627722b2127261
Makoto Chiba <hypermkt@gmail.com>
2018-11-15 23:23:50 +0900

    Implement by observer pattern.

---
+++ app/Observers/ArticleObserver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Observers;
+
+use App\DataAccess\Eloquent\Article;
+use App\Notifications\Slack;
+use App\Notifications\SlackNotifiable;
+
+class ArticleObserver
+{
+    /**
+     * Handle the article "created" event.
+     *
+     * @param  Article  $article
+     * @return void
+     */
+    public function created(Article $article)
+    {
+        (new SlackNotifiable())->notify(new Slack(
+            $article->title,
+            $article->article_url
+        ));
+    }
+}
