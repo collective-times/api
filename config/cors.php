10e9d8ca70f734b65b9c0786007d51ad3f4fe42d
Makoto Chiba <hypermkt@gmail.com>
2020-01-02 00:33:39 +0900

    laravel-corsの設定のキーがv1.0.0からsnake_caseに仕様変更となった

--- config/cors.php
+++ config/cors.php
@@ -12,12 +12,12 @@ return [
     |
     */
    
-    'supportsCredentials' => true,
-    'allowedOrigins' => ['*'],
-    'allowedOriginsPatterns' => [],
-    'allowedHeaders' => ['*'],
-    'allowedMethods' => ['*'],
-    'exposedHeaders' => [],
-    'maxAge' => 0,
+    'supports_credentials' => true,
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'allowed_methods' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
 
 ];
